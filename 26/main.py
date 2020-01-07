2ead761d2bf3a40d3e5ba5345c2823abd7f8b0ce
Record <record@Rafas-MacBook-Pro.local>
2020-01-07 23:54:04 +0100

    26

---
+++ 26/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "@@@@Hello, World@@@@"
+
+
+@app.route("/greetings/")
+@app.route("/greetings/<username>")
+def greetings(username="User"):
+    greetings = "Hi " + username
+    return greetings
+
+
+@app.route("/greetings/<name>/<surname>")
+def full_greeting(name, surname):
+    return f"Welcome {name.capitalize()} {surname.capitalize()}"
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    if request.form['name'] == "Admin" and request.form['password'] == "123":
+        return "Success!"
+    return "Failure"
+
+
+@app.route('/login_form')
+def login_form():
+    return render_template('layout_form.html', default_username="Provide username")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
