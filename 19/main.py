39bf55ebd656d9175e20ae75c268e441e982a3e7
Record <record@Rafas-MacBook-Pro.local>
2020-01-07 23:23:19 +0100

    19

---
+++ 19/main.py
@@ -0,0 +1,34 @@
+import view
+from bill import Bill
+
+
+def main():
+    bill = Bill()
+    action = "Start"
+
+    while action != "End":
+        action = input("What you want to do? [Add meal, Add service, Sum, Discount, Check, Save, End]: ")
+
+        if action == "Add meal":
+            name, price = view.ask_for_meal()
+            bill.add_meal(name, price)
+        elif action == "Add service":
+            name, price, guest_number = view.ask_for_service()
+            bill.add_service(name, price, guest_number)
+        elif action == "Sum":
+            print(bill.calculate())
+        elif action == "Discount":
+            discount = view.ask_for_discount()
+            print(bill.calculate_with_discount(discount))
+        elif action == "Check":
+            overall_sum, discount = view.ask_for_check_discount()
+            print(Bill.check_discount(overall_sum, discount))
+        elif action == "Save":
+            filename = view.ask_for_filename()
+            bill.print_to_file(filename)
+        elif action == "Access":
+            bill.entries = []
+
+
+if __name__ == '__main__':
+    main()
