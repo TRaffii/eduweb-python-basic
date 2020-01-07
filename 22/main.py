dbf1f68b3157500f4223809b079ddb248723ad13
Record <record@Rafas-MacBook-Pro.local>
2020-01-07 23:44:35 +0100

    22-finish

--- 22/main.py
+++ 22/main.py
@@ -5,4 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!!!"
+    raise ValueError
+    return "@@@@Hello, World@@@@"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
