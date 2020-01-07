2256ec559c17fa95c447c45741bf5d6fa9e33d74
Record <record@Rafas-MacBook-Pro.local>
2020-01-08 00:14:53 +0100

    30

---
+++ 30/meal.py
@@ -0,0 +1,14 @@
+from entry import Entry
+
+
+class Meal(Entry):
+
+    def __init__(self, name, price):
+        super().__init__(name, price)
+
+    @classmethod
+    def from_dict(cls, dictionary):
+        return_object = Meal(None, None)
+        for k, v in dictionary.items():
+            setattr(return_object, k, v)
+        return return_object
